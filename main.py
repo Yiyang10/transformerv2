6a7df1144507ed9ea1cf3b86efc29bd761b38f6d
Yiyang10 <117321259+Yiyang10@users.noreply.github.com>
2025-04-17 18:10:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def main():
                         help="训练集 DataFrame pickle 路径")
     parser.add_argument("--test_pickle",  required=True,
                         help="测试集  DataFrame pickle 路径")
-    parser.add_argument("--seq_length",      default=64,  type=int)
+    parser.add_argument("--sequence_length",      default=64,  type=int)
     parser.add_argument("--batch_size",   default=32,  type=int)
     parser.add_argument("--epochs",       default=12,  type=int)
     parser.add_argument("--lr",           default=5e-4, type=float)
@@ -88,7 +88,7 @@ def main():
     # —— DataLoaders
     train_loader, test_loader = get_data_loaders_by_segment(
         args.train_pickle, args.test_pickle,
-        seq_len=args.seq_len, batch_size=args.batch_size)
+        sequence_length=args.sequence_length, batch_size=args.batch_size)
 
     # —— Model
     input_dim = 35
